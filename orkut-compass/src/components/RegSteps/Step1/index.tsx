7adb8ebdb1c91fb93de1b52e5be1c4e27dbc3749
Nathan <nathanhs002@gmail.com>
2023-07-29 14:26:22 -0300

    Refino da Tela inicial, criação steps de Registro

---
+++ orkut-compass/src/components/RegSteps/Step1/index.tsx
@@ -0,0 +1,105 @@
+// Step1.tsx
+import React, { useState } from "react";
+import styles from "./styles.module.css";
+import Input from "../../Input";
+import Button from "../../ButtonRegister";
+
+interface Step1Props {
+  onNext: (data: Step1Data) => void;
+}
+
+export interface Step1Data {
+  name: string;
+  email: string;
+  birthDate: string;
+  city: string;
+  state: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const Step1: React.FC<Step1Props> = ({ onNext }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleNext = () => {
+    if (!name || !email || !birthDate || !city || !state || !password || !confirmPassword) {
+      setError("Preencha todos os campos");
+      return;
+    } else if (password !== confirmPassword) {
+      setError("As senhas não são iguais");
+      return;
+    }
+
+    setError("");
+    onNext({
+      name,
+      email,
+      birthDate,
+      city,
+      state,
+      password,
+      confirmPassword,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.label}>Passo 1: Informações de Conta</h2>
+      <div className={styles.content}>
+        <Input
+          type="text"
+          placeholder="Nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="date"
+          placeholder="Data de Nascimento"
+          value={birthDate}
+          onChange={(e) => setBirthDate(e.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Cidade"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Estado"
+          value={state}
+          onChange={(e) => setState(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Confirme a Senha"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        {error && <p className={styles.labelError}>{error}</p>}
+        <Button Text="Próximo" onClick={handleNext} />
+      </div>
+    </div>
+  );
+};
+
+export default Step1;
--- orkut-compass/src/pages/Register/index.tsx
+++ orkut-compass/src/pages/Register/index.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import "./styles.module.css"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Step1 from "../../components/RegSteps/Step1";
+import Step2 from "../../components/RegSteps/Step2";
 
-interface RegisterProps {
-  
-}
+const Register: React.FC = () => {
+  const [step, setStep] = useState(1);
+  const navigate = useNavigate();
 
-const Register: React.FC<RegisterProps> = () => {
+  const handleStep1Next = () => {
+    setStep(2);
+  };
+
+  const handleStep2Previous = () => {
+    setStep(1);
+  };
+
+  const handleStep2Complete = () => {  
+    setTimeout(() => {
+      alert("Usuário cadastrado com sucesso!");
+      navigate("/login"); // Redirecionar para a página de login após o cadastro bem-sucedido
+    }, 1000);
+  };
+
   return (
     <div>
-      Register
+      {step === 1 && <Step1 onNext={handleStep1Next} />}
+      {step === 2 && (
+        <Step2 onPrevious={handleStep2Previous} onComplete={handleStep2Complete} />
+      )}
     </div>
   );
 };
